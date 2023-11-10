0f136e4b38944bddb4e90234cffec16e55de11c2
José Lucas <111397299+joselucas1810@users.noreply.github.com>
2023-11-09 21:54:28 -0300

    ClasseProdutosVendasProdutos
    "fusão" entre as classes modelProdutos e ModelVendasProdutos

---
+++ src/main/java/a/vendasy2/model/ModelProdutosVendasProdutos.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package a.vendasy2.model;
+
+/**
+ *Classe "fusão" entre modelProdutos e modelVendasProdutos
+ * @author jose_
+ */
+public class ModelProdutosVendasProdutos {
+    private ModelProdutos modelProdutos;
+    private ModelVendasProdutos modelVendasProdutos;
+
+    /**
+     * @return the modelProdutos
+     */
+    public ModelProdutos getModelProdutos() {
+        return modelProdutos;
+    }
+
+    /**
+     * @param modelProdutos the modelProdutos to set
+     */
+    public void setModelProdutos(ModelProdutos modelProdutos) {
+        this.modelProdutos = modelProdutos;
+    }
+
+    /**
+     * @return the modelVendasProdutos
+     */
+    public ModelVendasProdutos getModelVendasProdutos() {
+        return modelVendasProdutos;
+    }
+
+    /**
+     * @param modelVendasProdutos the modelVendasProdutos to set
+     */
+    public void setModelVendasProdutos(ModelVendasProdutos modelVendasProdutos) {
+        this.modelVendasProdutos = modelVendasProdutos;
+    }
+}
