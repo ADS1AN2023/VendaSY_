71ce592fbba8fba47f94183a35a30743e8bf3acf
José Lucas <111397299+joselucas1810@users.noreply.github.com>
2023-11-20 23:09:54 -0300

    Update ControllerClienteTest.java
    Código completo

--- src/test/java/ControllerTest/ControllerClienteTest.java
+++ src/test/java/ControllerTest/ControllerClienteTest.java
@@ -2,28 +2,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
-/**
- *
- * @author jose_
- */
+package ControllerTest;
 import a.vendasy2.controller.ControllerCliente;
 import a.vendasy2.model.ModelCliente;
 import a.vendasy2.dao.DAOCliente;
+import java.util.ArrayList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.extension.ExtendWith;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
-
+/**
+ *
+ * @author jose_
+ */
 public class ControllerClienteTest {
-
-  private ControllerCliente controllerCliente;
+     private ControllerCliente controllerCliente;
     private DAOCliente daoCliente;
 
     @BeforeEach
@@ -74,4 +71,97 @@ public void testAtualizarClienteController() {
         // Verificar se o resultado é verdadeiro para indicar que o cliente foi excluído com sucesso
         assertTrue(resultado);
     }
+     @Test
+    public void testSalvarClienteController() {
+        // Crio um objeto ModelCliente para simular os dados do cliente a serem salvos
+        ModelCliente cliente = new ModelCliente();
+        cliente.setCliNome("Nome");
+        cliente.setCliEndereco("Endereço");
+        cliente.setCliBairro("Bairro");
+        cliente.setCliCidade("Cidade");
+        cliente.setCliUf("UF");
+        cliente.setCliCep("CEP");
+        cliente.setCliTelefone("Telefone");
+
+        // Configuro o comportamento do mock para simular a inserção bem-sucedida no banco de dados (retorna 1, indicando um ID de cliente válido)
+        when(daoCliente.salvarClienteDAO(cliente)).thenReturn(1);
+
+        // Invoco o método salvarClienteController() do MeuController
+        int resultadoSalvar = controllerCliente.salvarClienteController(cliente);
+
+        // Verifico se o resultado da inserção é igual a 1 (indicando sucesso)
+        assertEquals(1, resultadoSalvar);
+    }
+    @Test
+    public void testGetClienteControllerPId() {
+        int idCliente = 1; // Defino o ID do cliente para o teste
+
+        // Crio um objeto ModelCliente para simular os dados do cliente retornado pelo DAO
+        ModelCliente clienteRetornado = new ModelCliente();
+        clienteRetornado.setIdCliente(idCliente);
+        clienteRetornado.setCliNome("Nome");
+        clienteRetornado.setCliEndereco("Endereço");
+        clienteRetornado.setCliBairro("Bairro");
+        clienteRetornado.setCliCidade("Cidade");
+        clienteRetornado.setCliUf("UF");
+        clienteRetornado.setCliCep("CEP");
+        clienteRetornado.setCliTelefone("Telefone");
+
+        // Configuro o comportamento do mock para simular o retorno do cliente pelo DAO
+        when(daoCliente.getClienteDAO(idCliente)).thenReturn(clienteRetornado);
+
+        // Invoco o método getClienteController() do MeuController
+        ModelCliente resultadoCliente = controllerCliente.getClienteController(idCliente);
+
+        // Verifico se o cliente retornado pelo controller é igual ao cliente simulado retornado pelo DAO
+        assertEquals(clienteRetornado, resultadoCliente);
+    }
+     @Test
+    public void testGetClienteControllerPNome() {
+        String nomeCliente = "NomeCliente"; // Defino o nome do cliente para o teste
+
+        // Crio um objeto ModelCliente para simular os dados do cliente retornado pelo DAO
+        ModelCliente clienteRetornado = new ModelCliente();
+        clienteRetornado.setIdCliente(1);
+        clienteRetornado.setCliNome(nomeCliente);
+        clienteRetornado.setCliEndereco("Endereço");
+        clienteRetornado.setCliBairro("Bairro");
+        clienteRetornado.setCliCidade("Cidade");
+        clienteRetornado.setCliUf("UF");
+        clienteRetornado.setCliCep("CEP");
+        clienteRetornado.setCliTelefone("Telefone");
+
+        // Configuro o comportamento do mock para simular o retorno do cliente pelo DAO
+        when(daoCliente.getClienteDAO(nomeCliente)).thenReturn(clienteRetornado);
+
+        // Invoco o método getClienteController() do MeuController
+        ModelCliente resultadoCliente = controllerCliente.getClienteController(nomeCliente);
+
+        // Verifico se o cliente retornado pelo controller é igual ao cliente simulado retornado pelo DAO
+        assertEquals(clienteRetornado, resultadoCliente);
+    }
+     @Test
+    public void testGetListaClienteController() {
+        // Crio uma lista simulada de ModelCliente
+        ArrayList<ModelCliente> listaSimulada = new ArrayList<>();
+        for (int i = 1; i <= 3; i++) {
+            ModelCliente cliente = new ModelCliente();
+            cliente.setIdCliente(i);
+            cliente.setCliNome("Cliente " + i);
+            cliente.setCliEndereco("Endereço " + i);
+            // Defina outros atributos conforme necessário
+            listaSimulada.add(cliente);
+        }
+
+        // Configuro o comportamento do mock para simular o retorno da lista pelo DAO
+       Mockito.when(daoCliente.getListaClienteDAO()).thenReturn(listaSimulada);
+
+        // Invoco o método getListaClienteController() do MeuController
+        ArrayList<ModelCliente> resultadoLista = controllerCliente.getListaClienteController();
+
+        // Verifico se a lista retornada pelo controller é igual à lista simulada retornada pelo DAO
+        assertEquals(listaSimulada.size(), resultadoLista.size());
+        assertTrue(resultadoLista.containsAll(listaSimulada));
+    }
 }
+
